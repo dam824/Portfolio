7d12e348a3f02e70666fccae648589b13a4c83a4
dam824 <damiendagory@gmail.com>
2023-11-12 22:56:16 +0100

    maj email

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -11,7 +11,7 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["damiendagory@gmail.com", email],
+      to: ["contact@damiendagory.fr", email],
       subject: subject ,
       react: (
         <>
