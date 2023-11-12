2a124681dfdc6e611adc6e3a46a276fc15b99f82
dam824 <damiendagory@gmail.com>
2023-11-12 18:54:22 +0100

    ajout lien cv

--- app/components/HeaderSection.jsx
+++ app/components/HeaderSection.jsx
@@ -50,8 +50,10 @@ Photoshop, Procreate, Adobe Xd et Canva sont mes alliés de choix en design, cha
                     Contact
                     </Link>
                     <Link
-                    href="/"
+                    href="https://drive.google.com/file/d/1rW6atrWfXEvAvHAiZ1Uk0FiOp47zLsnr/view"
                     className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-400 hover:bg-slate-800 text-white mt-3"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     >
                      <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                      Download cv
