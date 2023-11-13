6d348d3950d9f4f7b2282142699c7db8e79150bd
dam824 <damiendagory@gmail.com>
2023-11-13 20:02:41 +0100

    maj section email

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -44,7 +44,7 @@ const EmailSection = () => {
             <h5 className='text-xl font-bold text-white my-2'> Me contacter</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {""}
-                Vous avez une idée de projet à concrétiser ? Besoin de conseil pour organiser et développer vos concepts ? N&apos;hésitez pas à me contacter. Je serais ravi de mettre à profit mon expertise et ma créativité pour transformer vos idées en réalités. Ensemble, explorons les possibilités et donnons vie à vos projets
+                Vous avez une idée de projet à concrétiser ? Besoin de conseil pour organiser et développer vos concepts ? N&apos;hésitez pas à me contacter. Je serai ravi de mettre à profit mon expertise et ma créativité pour transformer vos idées en réalités. Ensemble, explorons les possibilités et donnons vie à vos projets
             </p>
             <div className='socials flex flex-row gap-2'>
                 <Link href="https://github.com/dam824">
