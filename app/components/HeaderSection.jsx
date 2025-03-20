8083731fbffd777805db83a9ac4970a05e3da135
dam824 <damiendagory@gmail.com>
2025-03-20 13:08:37 +0100

    add vcf

--- app/components/HeaderSection.jsx
+++ app/components/HeaderSection.jsx
@@ -52,7 +52,7 @@ Besoin d’un site qui génère du business ou d’un audit SEO précis ? Contac
                     </p> 
                 <div>
                     <Link
-                    href="/contact."
+                    href="/contact.vcf"
                     download="contact.vcf"
                     className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
                     >
